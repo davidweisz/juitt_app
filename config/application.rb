f1a82e76818691b583571539f1287d669faedd3e
David Weisz <daweisz@gmail.com>
2012-02-05 13:57:06 +0200

    Removed default Rails page

--- config/application.rb
+++ config/application.rb
@@ -56,8 +56,8 @@ module JuittApp
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
+    # Disable the new the asset pipeline
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
