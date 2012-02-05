e7992820752b2e3c5f5262636f20e811c57395b8
David Weisz <daweisz@gmail.com>
2012-02-06 00:51:43 +0200

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -51,7 +51,12 @@ describe UsersController do
             post :create, :user => @attr
             flash[:success].should =~ /welcome to juitt/i
           end
-    end    
+          
+          it "should sign the user in" do
+            post :create, :user => @attr
+            controller.should be_signed_in
+          end
+     end    
   end
   
   describe "GET 'show'" do
@@ -83,9 +88,8 @@ describe UsersController do
           get :show, :id => @user
           response.should have_selector("h1>img", :class => "gravatar")
         end
-      end
-  end
-  
+  end 
+   
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
