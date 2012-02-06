c4639d0bbd56597956d23b66cd51130aa6b48141
David Weisz <daweisz@gmail.com>
2012-02-06 21:15:14 +0200

    New will_paginate version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Juitt - Share your movie library"
     if @title.nil?
       base_title
     else
@@ -10,6 +10,6 @@ module ApplicationHelper
   end
   
   def logo
-    image_tag("logo.png", :alt => "Sample App", :class => "round")
+    image_tag("logo.png", :alt => "Juitt logo", :class => "round")
   end
 end
